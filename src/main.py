b4746adbe21cc1acf64bc1c55970de83b71094e2
NEGU93 <joseagustin.barra@gmail.com>
2018-08-12 16:47:03 -0300

    changes to the notebook plus some coments and code tidyness

--- src/main.py
+++ src/main.py
@@ -21,10 +21,10 @@ flags.DEFINE_integer("output_width", None,
                      "The size of the output images to produce. If None, same value as output_height [None]")
 flags.DEFINE_string("dataset", "anime-faces", "The name of dataset [celebA, mnist, lsun]")
 flags.DEFINE_string("input_fname_pattern", "*.jpg", "Glob pattern of filename of input images [*]")
-flags.DEFINE_string("checkpoint_dir", "checkpoint", "Directory name to save the checkpoints [checkpoint]")
+flags.DEFINE_string("checkpoint_dir", "../checkpoint", "Directory name to save the checkpoints [checkpoint]")
 flags.DEFINE_string("data_dir", "../data", "Root directory of dataset [data]")
-flags.DEFINE_string("sample_dir", "samples", "Directory name to save the image samples [samples]")
-flags.DEFINE_boolean("train", True, "True for training, False for testing [False]")
+flags.DEFINE_string("sample_dir", "../samples", "Directory name to save the image samples [samples]")
+flags.DEFINE_boolean("train", False, "True for training, False for testing [False]")
 flags.DEFINE_boolean("crop", True, "True for training, False for testing [False]")
 flags.DEFINE_boolean("visualize", False, "True for visualizing, False for nothing [False]")
 flags.DEFINE_integer("generate_test_images", 100, "Number of images to generate during test. [100]")
@@ -32,57 +32,39 @@ FLAGS = flags.FLAGS
 
 
 def main(_):
-    pp.pprint(flags.FLAGS.__flags)
+    pp.pprint(flags.FLAGS.__flags)                  # Muestro los parámetros
 
     if FLAGS.input_width is None:
-        FLAGS.input_width = FLAGS.input_height
+        FLAGS.input_width = FLAGS.input_height      # Si no se especifica se asume imagen cuadrada
     if FLAGS.output_width is None:
         FLAGS.output_width = FLAGS.output_height
 
-    if not os.path.exists(FLAGS.checkpoint_dir):
+    if not os.path.exists(FLAGS.checkpoint_dir):    # Crea las carpetas si no existen
         os.makedirs(FLAGS.checkpoint_dir)
     if not os.path.exists(FLAGS.sample_dir):
         os.makedirs(FLAGS.sample_dir)
 
     # gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
-    run_config = tf.ConfigProto()
+    run_config = tf.ConfigProto()                   # Permite la alocación dinámica de memodia de la GPU.
     run_config.gpu_options.allow_growth = True
 
     with tf.Session(config=run_config) as sess:
-        if FLAGS.dataset == 'mnist':
-            dcgan = DCGAN(
-                sess,
-                input_width=FLAGS.input_width,
-                input_height=FLAGS.input_height,
-                output_width=FLAGS.output_width,
-                output_height=FLAGS.output_height,
-                batch_size=FLAGS.batch_size,
-                sample_num=FLAGS.batch_size,
-                y_dim=10,
-                z_dim=FLAGS.generate_test_images,
-                dataset_name=FLAGS.dataset,
-                input_fname_pattern=FLAGS.input_fname_pattern,
-                crop=FLAGS.crop,
-                checkpoint_dir=FLAGS.checkpoint_dir,
-                sample_dir=FLAGS.sample_dir,
-                data_dir=FLAGS.data_dir)
-        else:
-            dcgan = DCGAN(
-                sess,
-                input_width=FLAGS.input_width,
-                input_height=FLAGS.input_height,
-                output_width=FLAGS.output_width,
-                output_height=FLAGS.output_height,
-                batch_size=FLAGS.batch_size,
-                sample_num=FLAGS.batch_size,
-                z_dim=FLAGS.generate_test_images,
-                dataset_name=FLAGS.dataset,
-                input_fname_pattern=FLAGS.input_fname_pattern,
-                crop=FLAGS.crop,
-                checkpoint_dir=FLAGS.checkpoint_dir,
-                sample_dir=FLAGS.sample_dir,
-                data_dir=FLAGS.data_dir)
-
+        dcgan = DCGAN(
+            sess,
+            input_width=FLAGS.input_width,
+            input_height=FLAGS.input_height,
+            output_width=FLAGS.output_width,
+            output_height=FLAGS.output_height,
+            batch_size=FLAGS.batch_size,
+            sample_num=FLAGS.batch_size,
+            z_dim=FLAGS.generate_test_images,
+            dataset_name=FLAGS.dataset,
+            input_fname_pattern=FLAGS.input_fname_pattern,
+            crop=FLAGS.crop,
+            checkpoint_dir=FLAGS.checkpoint_dir,
+            sample_dir=FLAGS.sample_dir,
+            data_dir=FLAGS.data_dir
+        )
         show_all_variables()
 
         if FLAGS.train:
--- src/model.py
+++ src/model.py
@@ -22,7 +22,6 @@ class DCGAN(object):
                  gfc_dim=1024, dfc_dim=1024, c_dim=3, dataset_name='default',
                  input_fname_pattern='*.jpg', checkpoint_dir=None, sample_dir=None, data_dir='./data'):
         """
-
         Args:
           sess: TensorFlow session
           batch_size: The size of batch. Should be specified before training.
@@ -73,20 +72,16 @@ class DCGAN(object):
         self.checkpoint_dir = checkpoint_dir
         self.data_dir = data_dir
 
-        if self.dataset_name == 'mnist':
-            self.data_X, self.data_y = self.load_mnist()
-            self.c_dim = self.data_X[0].shape[-1]
+        data_path = os.path.join(self.data_dir, self.dataset_name, self.input_fname_pattern)
+        self.data = glob(data_path)
+        if len(self.data) == 0:
+            raise Exception("[!] No data found in '" + data_path + "'")
+        np.random.shuffle(self.data)
+        imreadImg = imread(self.data[0])
+        if len(imreadImg.shape) >= 3:  # check if image is a non-grayscale image by checking channel number
+            self.c_dim = imread(self.data[0]).shape[-1]
         else:
-            data_path = os.path.join(self.data_dir, self.dataset_name, self.input_fname_pattern)
-            self.data = glob(data_path)
-            if len(self.data) == 0:
-                raise Exception("[!] No data found in '" + data_path + "'")
-            np.random.shuffle(self.data)
-            imreadImg = imread(self.data[0])
-            if len(imreadImg.shape) >= 3:  # check if image is a non-grayscale image by checking channel number
-                self.c_dim = imread(self.data[0]).shape[-1]
-            else:
-                self.c_dim = 1
+            self.c_dim = 1
 
         self.grayscale = (self.c_dim == 1)
 
@@ -106,13 +101,11 @@ class DCGAN(object):
         else:
             image_dims = [self.input_height, self.input_width, self.c_dim]
 
-        self.inputs = tf.placeholder(
-            tf.float32, [self.batch_size] + image_dims, name='real_images')
+        self.inputs = tf.placeholder(tf.float32, [self.batch_size] + image_dims, name='real_images')
 
         inputs = self.inputs
 
-        self.z = tf.placeholder(
-            tf.float32, [None, self.z_dim], name='z')
+        self.z = tf.placeholder(tf.float32, [None, self.z_dim], name='z')
         self.z_sum = histogram_summary("z", self.z)
 
         self.G = self.generator(self.z, self.y)
@@ -130,12 +123,9 @@ class DCGAN(object):
             except:
                 return tf.nn.sigmoid_cross_entropy_with_logits(logits=x, targets=y)
 
-        self.d_loss_real = tf.reduce_mean(
-            sigmoid_cross_entropy_with_logits(self.D_logits, tf.ones_like(self.D)))
-        self.d_loss_fake = tf.reduce_mean(
-            sigmoid_cross_entropy_with_logits(self.D_logits_, tf.zeros_like(self.D_)))
-        self.g_loss = tf.reduce_mean(
-            sigmoid_cross_entropy_with_logits(self.D_logits_, tf.ones_like(self.D_)))
+        self.d_loss_real = tf.reduce_mean(sigmoid_cross_entropy_with_logits(self.D_logits, tf.ones_like(self.D)))
+        self.d_loss_fake = tf.reduce_mean(sigmoid_cross_entropy_with_logits(self.D_logits_, tf.zeros_like(self.D_)))
+        self.g_loss = tf.reduce_mean(sigmoid_cross_entropy_with_logits(self.D_logits_, tf.ones_like(self.D_)))
 
         self.d_loss_real_sum = scalar_summary("d_loss_real", self.d_loss_real)
         self.d_loss_fake_sum = scalar_summary("d_loss_fake", self.d_loss_fake)
@@ -162,20 +152,14 @@ class DCGAN(object):
         except:
             tf.initialize_all_variables().run()
 
-        self.g_sum = merge_summary([self.z_sum, self.d__sum,
-                                    self.G_sum, self.d_loss_fake_sum, self.g_loss_sum])
-        self.d_sum = merge_summary(
-            [self.z_sum, self.d_sum, self.d_loss_real_sum, self.d_loss_sum])
-        self.writer = SummaryWriter("./logs", self.sess.graph)
+        self.g_sum = merge_summary([self.z_sum, self.d__sum, self.G_sum, self.d_loss_fake_sum, self.g_loss_sum])
+        self.d_sum = merge_summary([self.z_sum, self.d_sum, self.d_loss_real_sum, self.d_loss_sum])
+        self.writer = SummaryWriter("../logs", self.sess.graph)
 
         sample_z = np.random.uniform(-1, 1, size=(self.sample_num, self.z_dim))
 
-        if config.dataset == 'mnist':
-            sample_inputs = self.data_X[0:self.sample_num]
-            sample_labels = self.data_y[0:self.sample_num]
-        else:
-            sample_files = self.data[0:self.sample_num]
-            sample = [
+        sample_files = self.data[0:self.sample_num]
+        sample = [
                 get_image(sample_file,
                           input_height=self.input_height,
                           input_width=self.input_width,
@@ -183,10 +167,10 @@ class DCGAN(object):
                           resize_width=self.output_width,
                           crop=self.crop,
                           grayscale=self.grayscale) for sample_file in sample_files]
-            if (self.grayscale):
-                sample_inputs = np.array(sample).astype(np.float32)[:, :, :, None]
-            else:
-                sample_inputs = np.array(sample).astype(np.float32)
+        if (self.grayscale):
+            sample_inputs = np.array(sample).astype(np.float32)[:, :, :, None]
+        else:
+            sample_inputs = np.array(sample).astype(np.float32)
 
         counter = 1
         start_time = time.time()
@@ -198,21 +182,14 @@ class DCGAN(object):
             print(" [!] Load failed...")
 
         for epoch in xrange(config.epoch):
-            if config.dataset == 'mnist':
-                batch_idxs = min(len(self.data_X), config.train_size) // config.batch_size
-            else:
-                self.data = glob(os.path.join(
-                    config.data_dir, config.dataset, self.input_fname_pattern))
-                np.random.shuffle(self.data)
-                batch_idxs = min(len(self.data), config.train_size) // config.batch_size
+            self.data = glob(os.path.join(
+                config.data_dir, config.dataset, self.input_fname_pattern))
+            np.random.shuffle(self.data)
+            batch_idxs = min(len(self.data), config.train_size) // config.batch_size
 
             for idx in xrange(0, int(batch_idxs)):
-                if config.dataset == 'mnist':
-                    batch_images = self.data_X[idx * config.batch_size:(idx + 1) * config.batch_size]
-                    batch_labels = self.data_y[idx * config.batch_size:(idx + 1) * config.batch_size]
-                else:
-                    batch_files = self.data[idx * config.batch_size:(idx + 1) * config.batch_size]
-                    batch = [
+                batch_files = self.data[idx * config.batch_size:(idx + 1) * config.batch_size]
+                batch = [
                         get_image(batch_file,
                                   input_height=self.input_height,
                                   input_width=self.input_width,
@@ -220,101 +197,51 @@ class DCGAN(object):
                                   resize_width=self.output_width,
                                   crop=self.crop,
                                   grayscale=self.grayscale) for batch_file in batch_files]
-                    if self.grayscale:
-                        batch_images = np.array(batch).astype(np.float32)[:, :, :, None]
-                    else:
-                        batch_images = np.array(batch).astype(np.float32)
+                if self.grayscale:
+                    batch_images = np.array(batch).astype(np.float32)[:, :, :, None]
+                else:
+                    batch_images = np.array(batch).astype(np.float32)
 
                 batch_z = np.random.uniform(-1, 1, [config.batch_size, self.z_dim]) \
                     .astype(np.float32)
 
-                if config.dataset == 'mnist':
-                    # Update D network
-                    _, summary_str = self.sess.run([d_optim, self.d_sum],
-                                                   feed_dict={
-                                                       self.inputs: batch_images,
-                                                       self.z: batch_z,
-                                                       self.y: batch_labels,
-                                                   })
-                    self.writer.add_summary(summary_str, counter)
-
-                    # Update G network
-                    _, summary_str = self.sess.run([g_optim, self.g_sum],
-                                                   feed_dict={
-                                                       self.z: batch_z,
-                                                       self.y: batch_labels,
-                                                   })
-                    self.writer.add_summary(summary_str, counter)
-
-                    # Run g_optim twice to make sure that d_loss does not go to zero (different from paper)
-                    _, summary_str = self.sess.run([g_optim, self.g_sum],
-                                                   feed_dict={self.z: batch_z, self.y: batch_labels})
-                    self.writer.add_summary(summary_str, counter)
-
-                    errD_fake = self.d_loss_fake.eval({
-                        self.z: batch_z,
-                        self.y: batch_labels
-                    })
-                    errD_real = self.d_loss_real.eval({
-                        self.inputs: batch_images,
-                        self.y: batch_labels
-                    })
-                    errG = self.g_loss.eval({
-                        self.z: batch_z,
-                        self.y: batch_labels
-                    })
-                else:
-                    # Update D network
-                    _, summary_str = self.sess.run([d_optim, self.d_sum],
+                # Update D network
+                _, summary_str = self.sess.run([d_optim, self.d_sum],
                                                    feed_dict={self.inputs: batch_images, self.z: batch_z})
-                    self.writer.add_summary(summary_str, counter)
+                self.writer.add_summary(summary_str, counter)
 
-                    # Update G network
-                    _, summary_str = self.sess.run([g_optim, self.g_sum],
+                # Update G network
+                _, summary_str = self.sess.run([g_optim, self.g_sum],
                                                    feed_dict={self.z: batch_z})
-                    self.writer.add_summary(summary_str, counter)
+                self.writer.add_summary(summary_str, counter)
 
-                    # Run g_optim twice to make sure that d_loss does not go to zero (different from paper)
-                    _, summary_str = self.sess.run([g_optim, self.g_sum],
+                # Run g_optim twice to make sure that d_loss does not go to zero (different from paper)
+                _, summary_str = self.sess.run([g_optim, self.g_sum],
                                                    feed_dict={self.z: batch_z})
-                    self.writer.add_summary(summary_str, counter)
+                self.writer.add_summary(summary_str, counter)
 
-                    errD_fake = self.d_loss_fake.eval({self.z: batch_z})
-                    errD_real = self.d_loss_real.eval({self.inputs: batch_images})
-                    errG = self.g_loss.eval({self.z: batch_z})
+                errD_fake = self.d_loss_fake.eval({self.z: batch_z})
+                errD_real = self.d_loss_real.eval({self.inputs: batch_images})
+                errG = self.g_loss.eval({self.z: batch_z})
 
                 counter += 1
                 print("Epoch: [%2d/%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
-                      % (epoch, config.epoch, idx, batch_idxs,
-                         time.time() - start_time, errD_fake + errD_real, errG))
+                      % (epoch, config.epoch, idx, batch_idxs, time.time() - start_time, errD_fake + errD_real, errG))
 
                 if np.mod(counter, 100) == 1:
-                    if config.dataset == 'mnist':
+                    try:
                         samples, d_loss, g_loss = self.sess.run(
                             [self.sampler, self.d_loss, self.g_loss],
                             feed_dict={
-                                self.z: sample_z,
-                                self.inputs: sample_inputs,
-                                self.y: sample_labels,
-                            }
+                                    self.z: sample_z,
+                                    self.inputs: sample_inputs,
+                                },
                         )
                         save_images(samples, image_manifold_size(samples.shape[0]),
                                     './{}/train_{:02d}_{:04d}.png'.format(config.sample_dir, epoch, idx))
                         print("[Sample] d_loss: %.8f, g_loss: %.8f" % (d_loss, g_loss))
-                    else:
-                        try:
-                            samples, d_loss, g_loss = self.sess.run(
-                                [self.sampler, self.d_loss, self.g_loss],
-                                feed_dict={
-                                    self.z: sample_z,
-                                    self.inputs: sample_inputs,
-                                },
-                            )
-                            save_images(samples, image_manifold_size(samples.shape[0]),
-                                        './{}/train_{:02d}_{:04d}.png'.format(config.sample_dir, epoch, idx))
-                            print("[Sample] d_loss: %.8f, g_loss: %.8f" % (d_loss, g_loss))
-                        except:
-                            print("one pic error!...")
+                    except:
+                        print("one pic error!...")
 
                 if np.mod(counter, 500) == 2:
                     self.save(config.checkpoint_dir, counter)
@@ -360,11 +287,9 @@ class DCGAN(object):
                 s_h16, s_w16 = conv_out_size_same(s_h8, 2), conv_out_size_same(s_w8, 2)
 
                 # project `z` and reshape
-                self.z_, self.h0_w, self.h0_b = linear(
-                    z, self.gf_dim * 8 * s_h16 * s_w16, 'g_h0_lin', with_w=True)
+                self.z_, self.h0_w, self.h0_b = linear(z, self.gf_dim * 8 * s_h16 * s_w16, 'g_h0_lin', with_w=True)
 
-                self.h0 = tf.reshape(
-                    self.z_, [-1, s_h16, s_w16, self.gf_dim * 8])
+                self.h0 = tf.reshape(self.z_, [-1, s_h16, s_w16, self.gf_dim * 8])
                 h0 = tf.nn.relu(self.g_bn0(self.h0))
 
                 self.h1, self.h1_w, self.h1_b = deconv2d(
@@ -460,43 +385,6 @@ class DCGAN(object):
                 h2 = conv_cond_concat(h2, yb)
 
                 return tf.nn.sigmoid(deconv2d(h2, [self.batch_size, s_h, s_w, self.c_dim], name='g_h3'))
-
-    def load_mnist(self):
-        data_dir = os.path.join(self.data_dir, self.dataset_name)
-
-        fd = open(os.path.join(data_dir, 'train-images-idx3-ubyte'))
-        loaded = np.fromfile(file=fd, dtype=np.uint8)
-        trX = loaded[16:].reshape((60000, 28, 28, 1)).astype(np.float)
-
-        fd = open(os.path.join(data_dir, 'train-labels-idx1-ubyte'))
-        loaded = np.fromfile(file=fd, dtype=np.uint8)
-        trY = loaded[8:].reshape((60000)).astype(np.float)
-
-        fd = open(os.path.join(data_dir, 't10k-images-idx3-ubyte'))
-        loaded = np.fromfile(file=fd, dtype=np.uint8)
-        teX = loaded[16:].reshape((10000, 28, 28, 1)).astype(np.float)
-
-        fd = open(os.path.join(data_dir, 't10k-labels-idx1-ubyte'))
-        loaded = np.fromfile(file=fd, dtype=np.uint8)
-        teY = loaded[8:].reshape((10000)).astype(np.float)
-
-        trY = np.asarray(trY)
-        teY = np.asarray(teY)
-
-        X = np.concatenate((trX, teX), axis=0)
-        y = np.concatenate((trY, teY), axis=0).astype(np.int)
-
-        seed = 547
-        np.random.seed(seed)
-        np.random.shuffle(X)
-        np.random.seed(seed)
-        np.random.shuffle(y)
-
-        y_vec = np.zeros((len(y), self.y_dim), dtype=np.float)
-        for i, label in enumerate(y):
-            y_vec[i, y[i]] = 1.0
-
-        return X / 255., y_vec
 
     @property
     def model_dir(self):
